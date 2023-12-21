e3fb16691cf3c659126c8a66306e9e7d32a86293
Trương Nghĩa <95394658+NghiaNon@users.noreply.github.com>
2023-12-21 14:52:32 +0700

    change_backend

--- msgbox.php
+++ msgbox.php
@@ -3,7 +3,7 @@
 if (isset($_SESSION['success'])) { ?>
     <div class="alert alert-success alert-dismissible" id='hideMe'>
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <h4><i class="icon fa fa-check"></i> Success!</h4>
+        <h4><i class="icon fa fa-check"></i> Thành công!</h4>
         <?php echo $_SESSION['success']; ?>
     </div>
 <?php
@@ -13,7 +13,7 @@ if (isset($_SESSION['success'])) { ?>
 if (isset($_SESSION['error'])) { ?>
     <div class="alert alert-danger alert-dismissible">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+        <h4><i class="icon fa fa-ban"></i> Thất bại!</h4>
         <?php echo $_SESSION['error']; ?>
     </div>
 <?php
