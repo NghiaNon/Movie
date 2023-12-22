f2b6ebf28bcf85dcd902319f9eea99c9b2e38a52
LHThach <lht2932002@gmail.com>
2023-12-22 18:42:30 +0700

    format admin

--- admin/pages/header.php
+++ admin/pages/header.php
@@ -1,8 +1,6 @@
-
 <?php
 session_start();
-if(!isset($_SESSION['admin']))
-{
+if (!isset($_SESSION['admin'])) {
   header('location:../index.php');
 }
 date_default_timezone_set('Asia/Kolkata');
@@ -10,13 +8,14 @@ include('../../config.php');
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Admin1 | Admin1</title>
   <!-- valodation -->
   <script type="text/javascript" src="../validation/vendor/jquery/jquery-1.10.2.min.js"></script>
-  <link rel="stylesheet" href="../validation/dist/css/bootstrapValidator.css"/> 
+  <link rel="stylesheet" href="../validation/dist/css/bootstrapValidator.css" />
   <script type="text/javascript" src="../validation/dist/js/bootstrapValidator.js"></script>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
@@ -29,7 +28,7 @@ include('../../config.php');
   <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
+      folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -39,107 +38,105 @@ include('../../config.php');
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 </head>
+
 <body class="hold-transition skin-blue sidebar-mini">
-<!-- Site wrapper -->
-<div class="wrapper">
+  <!-- Site wrapper -->
+  <div class="wrapper">
 
-  <header class="main-header">
-    <!-- Logo -->
-    <a href="index.php" class="logo">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>A</b>D</span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>QUẢN TRỊ VIÊN</b></span>
-    </a>
-    <!-- Header Navbar: style can be found in header.less -->
-    <nav class="navbar navbar-static-top">
-      <!-- Sidebar toggle button-->
-      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+    <header class="main-header">
+      <!-- Logo -->
+      <a href="index.php" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>A</b>D</span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>QUẢN TRỊ VIÊN</b></span>
       </a>
+      <!-- Header Navbar: style can be found in header.less -->
+      <nav class="navbar navbar-static-top">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
 
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-          <!-- Messages: style can be found in dropdown.less-->
-          <li class="dropdown user user-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="admin-icn.png" class="user-image" alt="User Image">
-              <span class="hidden-xs">Quản trị viên</span>
-            </a>
-            <ul class="dropdown-menu">
-              <!-- User image -->
-              <li class="user-header">
-                <img src="admin-icn.png" class="img-circle" alt="User Image">
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav">
+            <!-- Messages: style can be found in dropdown.less-->
+            <li class="dropdown user user-menu">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <img src="admin-icn.png" class="user-image" alt="User Image">
+                <span class="hidden-xs">Quản trị viên</span>
+              </a>
+              <ul class="dropdown-menu">
+                <!-- User image -->
+                <li class="user-header">
+                  <img src="admin-icn.png" class="img-circle" alt="User Image">
 
-                <p>
-                  Quản trị viên
-                </p>
-              </li>
-              <li class="user-footer">
-                <div class="pull-right">
-                  <a href="logout.php" class="btn btn-default btn-flat">Đăng xuất</a>
-                </div>
-              </li>
-            </ul>
-          </li>
-          <!-- Control Sidebar Toggle Button -->
-          <li>
-            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-
-  <!-- Left side column. contains the sidebar -->
-  <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
-      <!-- Sidebar user panel -->
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img src="admin-icn.png" class="img-circle" alt="User Image">
+                  <p>
+                    Quản trị viên
+                  </p>
+                </li>
+                <li class="user-footer">
+                  <div class="pull-right">
+                    <a href="logout.php" class="btn btn-default btn-flat">Đăng xuất</a>
+                  </div>
+                </li>
+              </ul>
+            </li>
+            <!-- Control Sidebar Toggle Button -->
+            <li>
+              <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+            </li>
+          </ul>
         </div>
-        <div class="pull-left info">
-          <p>Administrator</p>
-          <a href="#"><i class="fa fa-circle text-success"></i> Trực tuyến</a>
+      </nav>
+    </header>
+
+    <!-- Left side column. contains the sidebar -->
+    <aside class="main-sidebar">
+      <!-- sidebar: style can be found in sidebar.less -->
+      <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+          <div class="pull-left image">
+            <img src="admin-icn.png" class="img-circle" alt="User Image">
+          </div>
+          <div class="pull-left info">
+            <p>Administrator</p>
+            <a href="#"><i class="fa fa-circle text-success"></i> Trực tuyến</a>
+          </div>
         </div>
-      </div>
-      
-      <!-- /.search form -->
-      <!-- sidebar menu: : style can be found in sidebar.less -->
-      <ul class="sidebar-menu">
-        <li class="treeview">
-          <a href="index.php">
-            <i class="fa fa-home"></i> <span>Trang chủ</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-        </li>
-        
+
+        <ul class="sidebar-menu">
           <li class="treeview">
-          <a href="add_theatre.php">
-            <i class="fa fa-film"></i> <span>Thêm phim</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-        </li>
-        <li class="treeview">
-          <a href="add_movie_news.php">
-            <i class="fa fa-plus"></i> <span>Phim sắp ra mắt</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-        </li>
-        
-        
-      </ul>
-    </section>
-    <!-- /.sidebar -->
-  </aside>
+            <a href="index.php">
+              <i class="fa fa-home"></i> <span>Trang chủ</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+          </li>
+
+          <li class="treeview">
+            <a href="add_theatre.php">
+              <i class="fa fa-film"></i> <span>Thêm phim</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+          </li>
+          <li class="treeview">
+            <a href="add_movie_news.php">
+              <i class="fa fa-plus"></i> <span>Phim sắp ra mắt</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+          </li>
+
+
+        </ul>
+      </section>
+    </aside>
