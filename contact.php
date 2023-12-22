a6df738502f5df43829d0a25473da305b38749dd
LHThach <lht2932002@gmail.com>
2023-12-22 16:43:01 +0700

    sua contact

--- contact.php
+++ contact.php
@@ -110,45 +110,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			</div>
 		</div>
 	</div>
-	<div class="footer">
-		<div class="wrap">
-			<div class="footer-top">
-				<div class="col_1_of_4 span_1_of_4">
-					<div class="footer-nav">
-						<!-- <ul>
-							<li><a href="">Mẫu có sẵn</a></li>
-							<li><a href="">Mẫu có sẵn</a></li>
-							<li><a href="">Mẫu có sẵn</a></li>
-							<li><a href="">Mẫu có sẵn</a></li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<div class="textcontact">
-						<p>Cộng tác viên,<br>
-							Tổng đài viên<br>
-							SĐT: +1-125-234-456789.<br>
-							Email:hotro(at)hanoi.com<br>
-						</p>
-					</div>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<div class="call_info">
-						<p class="txt_3">Tư vấn miễn phí:</p>
-						<p class="txt_4">1 800 234 23456</p>
-					</div>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<div class=social>
-						<a href="https://www.facebook.com/"><img src="images/fb.png" alt="" /></a>
-						<a href="https://twitter.com/"><img src="images/tw.png" alt="" /></a>
-						
-					</div>
-				</div>
-				<div class="clear"></div>
-			</div>
-		</div>
-	</div>
+	
+	<?php include('footer.php'); ?>
 	<div class="footer-bottom">
 		<div class="wrap">
 		
